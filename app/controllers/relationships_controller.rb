ca767c63b6be702fbc2bbef25c8d0733d5518d11
kaino <kaifumi1001@gmail.com>
2020-03-08 09:20:35 +0000

    follow_ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,26 @@
 class RelationshipsController < ApplicationController
+  before_action :logged_in_user
+
+  def create
+
+    @user=User.find(params[:followed_id])
+    current_user.follow(@user)
+    # formatがhtmlかjsによって出し方が異なる
+    respond_to do |format|
+      format.html {redirect_to @user}
+      format.js
+    end
+    # redirect_to user
+  end
+
+  def destroy
+    # Relationshipテーブルから受け取ったidに一致するレコードを探して、followedに対応するものを抽出
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html {redirect_to @user}
+      format.js
+    end
+    # redirect_to user
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :baria_user, only: [:update]
+	before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 
   def show
   	@user = User.find(params[:id])
@@ -8,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def index
-	@user=User.find(current_user.id)
+		@user=current_user
   	@users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
   	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
   end
@@ -40,10 +41,18 @@ class UsersController < ApplicationController
   end
 
   #url直接防止　メソッドを自己定義してbefore_actionで発動。
-   def baria_user
-  	unless params[:id].to_i == current_user.id
+   	def baria_user
+  		unless params[:id].to_i == current_user.id
   		redirect_to user_path(current_user)
-  	end
-   end
+  		end
+	 	end
+	 
+		#  application.controller.rbに設置したので不要
+		# def logged_in_user
+		# 	unless logged_in?
+		# 		falsh[:danger]="Please log in."
+		# 		redirect_to new_user_session_path
+		# 	end
+		# end
 
 end
