a36832075558ad7c146471edc8b099bb4de74ecf
kaino <kaifumi1001@gmail.com>
2020-03-09 05:02:39 +0000

    [finish]asynchronous_favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-    @favorite = current_user.favorites.create(book_id: params[:book_id])
-    redirect_back(fallback_location: root_path)
+    @book=Book.find(params[:book_id])
+    @favorite = current_user.favorites.new(book_id: params[:book_id])
+    @favorite.save
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @book=Book.find(params[:book_id])
     @favorite = Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
     @favorite.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 end
